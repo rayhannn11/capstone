6177b3478e0808a915a12b0f249ab033d4afcbc2
rayhannn11 <rayhan.onepiece@gmail.com>
2023-10-21 17:24:26 +0700

    add favorite exercises

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -11,8 +11,8 @@ const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
 
   return (
-    <Typography onClick={() => scrollPrev()} className="right-arrow">
-      <img src={LeftArrowIcon} alt="right-arrow" />
+    <Typography onClick={() => scrollPrev()} className='right-arrow'>
+      <img src={LeftArrowIcon} alt='right-arrow' />
     </Typography>
   );
 };
@@ -21,25 +21,35 @@ const RightArrow = () => {
   const { scrollNext } = useContext(VisibilityContext);
 
   return (
-    <Typography onClick={() => scrollNext()} className="left-arrow">
-      <img src={RightArrowIcon} alt="right-arrow" />
+    <Typography onClick={() => scrollNext()} className='left-arrow'>
+      <img src={RightArrowIcon} alt='right-arrow' />
     </Typography>
   );
 };
 
-const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
-  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-    {data.map((item) => (
-      <Box
-        key={item.id || item}
-        itemId={item.id || item}
-        title={item.id || item}
-        m="0 40px"
-      >
-        {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
-      </Box>
-    ))}
-  </ScrollMenu>
-);
+const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => {
+  return (
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+      {data.map((item) => (
+        <Box
+          key={item.id || item}
+          itemId={item.id || item}
+          title={item.id || item}
+          m='0 40px'
+        >
+          {bodyParts ? (
+            <BodyPart
+              item={item}
+              setBodyPart={setBodyPart}
+              bodyPart={bodyPart}
+            />
+          ) : (
+            <ExerciseCard exercise={item} />
+          )}
+        </Box>
+      ))}
+    </ScrollMenu>
+  );
+};
 
 export default HorizontalScrollbar;
